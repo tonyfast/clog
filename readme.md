9da33fa9e15a1fde5bcd6a05ea56ea4fd347be16
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2017-09-25 01:12:00 +0000

    Deploy tonyfast/clog to github.com/tonyfast/clog.git:gh-pages

---
+++ readme.md
@@ -0,0 +1,21 @@
+
+# [<small>code and works</small>](https://tonyfast.github.io/clog)
+
+<code>pip install git+<a href="https://github.com/tonyfast/clog">https://github.com/tonyfast/clog</a></code>
+
+
+```python
+from clog import *
+```
+
+# Build
+
+
+```python
+!jupyter nbconvert --execute index.ipynb
+```
+
+    [NbConvertApp] Converting notebook index.ipynb to html
+    [NbConvertApp] Executing notebook with kernel: python3
+    [NbConvertApp] Writing 254788 bytes to index.html
+
---
+++ setup.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+import setuptools
+
+setuptools.setup(
+    name="clog",
+    version="0.0.1",
+    author="Tony Fast",
+    author_email="tony.fast@gmail.com",
+    description="",
+    license="BSD-3-Clause",
+    url="http://github.com/tonyfast/clog",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Topic :: Utilities",
+        "Framework :: IPython",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "Intended Audience :: Developers",
+        "Development Status :: 3 - Alpha",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Topic :: Software Development :: Documentation",
+    ],
+    install_requires=[],
+    tests_require=[],)
